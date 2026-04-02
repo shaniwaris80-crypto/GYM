93d2fa429420e7c5e31e32f89cc00f940903f05e
shaniwaris80-crypto <shaniwaris80@gmail.com>
2026-04-02 16:52:59 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+window.appConfig = {
+  useFirebase: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCthnUFk8yFa1hg1k_-5qiZmducWmVENS4",
+    authDomain: "gimnasio-f48a7.firebaseapp.com",
+    projectId: "gimnasio-f48a7",
+    storageBucket: "gimnasio-f48a7.firebasestorage.app",
+    messagingSenderId: "536332136790",
+    appId: "1:536332136790:web:82f0cf0ebdf62cae69e6f7",
+    measurementId: "G-5ES0DBT5YD"
+  }
+};
